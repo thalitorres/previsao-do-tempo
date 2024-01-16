6fd010b1a7bf144932a7aebcd7c94f3d97f35799
Thalita Torres <111453398+thalitorres@users.noreply.github.com>
2024-01-16 09:09:53 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const key = "a49b63dc901f7025c1ce70b932d798aa";
+
+function colocarDadosNaTela(dados){
+
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+}
+
+async function buscarCidade(cidade){
+    
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+
+    colocarDadosNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
